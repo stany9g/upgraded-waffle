c7dc3effb3f6984be73a967740909dd6a5fd018e
Stany <stany.caja@gmail.com>
2020-09-19 10:38:15 +0200

    Add task 2

---
+++ Task 2 - Parties/Meeting.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Meetings
+{
+    public class MeetingClass
+    {
+        public class Meeting
+        {
+            public int StartTime { get; set; }
+
+            public int EndTime { get; set; }
+
+            public Meeting(int startTime, int endTime)
+            {
+                // Number of 30 min blocks past 9:00 am
+                StartTime = startTime;
+                EndTime = endTime;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null || GetType() != obj.GetType())
+                {
+                    return false;
+                }
+
+                if (ReferenceEquals(obj, this))
+                {
+                    return true;
+                }
+
+                var meeting = (Meeting)obj;
+                return StartTime == meeting.StartTime && EndTime == meeting.EndTime;
+            }
+
+            public override int GetHashCode()
+            {
+                var result = 17;
+                result = result * 31 + StartTime;
+                result = result * 31 + EndTime;
+                return result;
+            }
+
+            public override string ToString()
+            {
+                return $"({StartTime}, {EndTime})";
+            }
+        }
+
+        public static List<Meeting> MergeRanges(List<Meeting> meetings)
+        {
+            // Implement me
+	    return new List<Meeting>();
+        }
+    }
+}
