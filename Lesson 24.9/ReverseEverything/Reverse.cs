1ae9187d08db6e2ef3498388ecc33bba9d1f8da4
Stany <stany.caja@gmail.com>
2020-10-01 15:00:16 +0200

    Add Reverse solution

--- Lesson 24.9/ReverseEverything/Reverse.cs
+++ Lesson 24.9/ReverseEverything/Reverse.cs
@@ -4,12 +4,47 @@
     {
         public static void ReverseChars(char[] arrayOfChars)
         {
-            // Implement me
+            int leftIndex = 0;
+            int rightIndex = arrayOfChars.Length - 1;
+
+            while (leftIndex < rightIndex)
+            {
+                // Swap characters
+                char temp = arrayOfChars[leftIndex];
+                arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+                arrayOfChars[rightIndex] = temp;
+
+                // Move towards middle
+                leftIndex++;
+                rightIndex--;
+            }
         }
 
         public static void ReverseWords(char[] message)
         {
-            // Implement me
+            ReverseCharacters(message, 0, message.Length - 1);
+
+            int currentWordStartIndex = 0;
+            for (int i = 0; i <= message.Length; i++)
+            {
+                if (i == message.Length || message[i] == ' ')
+                {
+                    ReverseCharacters(message, currentWordStartIndex, i - 1);
+                    currentWordStartIndex = i + 1;
+                }
+            }
+        }
+
+        public static void ReverseCharacters(char[] message, int leftIndex, int rightIndex)
+        {
+            while (leftIndex < rightIndex)
+            {
+                char temp = message[leftIndex];
+                message[leftIndex] = message[rightIndex];
+                message[rightIndex] = temp;
+                leftIndex++;
+                rightIndex--;
+            }
         }
     }
 }
