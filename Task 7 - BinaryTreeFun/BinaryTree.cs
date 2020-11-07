bb7f6969986d565a135bbd5a3526bc9a8d75dbbe
Stany <stany.caja@gmail.com>
2020-11-07 11:49:22 +0100

    Add Task 7 - BinaryTreeFun

---
+++ Task 7 - BinaryTreeFun/BinaryTree.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace BinaryTreeFun
+{
+    public class BinaryTree
+    {
+        public class BinaryTreeNode
+        {
+            public int Value { get; }
+
+            public BinaryTreeNode Left { get; private set; }
+
+            public BinaryTreeNode Right { get; private set; }
+
+            public BinaryTreeNode(int value)
+            {
+                Value = value;
+            }
+
+            public BinaryTreeNode InsertLeft(int leftValue)
+            {
+                Left = new BinaryTreeNode(leftValue);
+                return Left;
+            }
+
+            public BinaryTreeNode InsertRight(int rightValue)
+            {
+                Right = new BinaryTreeNode(rightValue);
+                return Right;
+            }
+
+        }
+
+
+        public static bool IsBalanced(BinaryTreeNode root)
+        {
+            return false;
+        }
+
+        public static bool IsBinarySearchTree(BinaryTreeNode root)
+        {
+            return false;
+        }
+
+        public static int FindSecondLargest(BinaryTreeNode root)
+        {
+            return 0;
+        }
+    }
+
+
+}
---
+++ Task 7 - BinaryTreeFun/README.md
@@ -0,0 +1,18 @@
+# Task - BinaryTreeFun
+1. Download *BinaryTree.cs*
+2. Implement *IsBalanced* method
+2. Implement *IsBinarySearchTree* method
+2. Implement *FindSecondLargest* method
+3. Zip the *BinaryTree.cs* without Main method if case
+4. Ship it!
+
+### Balanced
+It is balanced when the difference in depths between any two leaf nodes is not bigger than one.
+
+### IsBinarySearch
+A binary search tree is a binary tree where two conditions are met:
+- The tree nodes to the left are smaller than its parent.
+- The tree nodes to the right are larger than its parent.
+
+### FindSecondLargest
+Find the second largest item in the binary search tree.
